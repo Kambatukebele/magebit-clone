6c678a2631ce2acec563322e71523ef2c4f9af05
Kambatukebele <kambacharles11@gmail.com>
2025-03-25 16:43:24 +0100

    added case studies icons and images and javascript

--- src/js/main.js
+++ src/js/main.js
@@ -24,7 +24,6 @@ const toggleMenuOnSmallDevice = () => {
   hamburgerMenu.addEventListener("click", handleToggleMenu);
   closeMenu.addEventListener("click", handleToggleMenu);
 };
-
 toggleMenuOnSmallDevice();
 
 // JOurney
@@ -121,5 +120,115 @@ const onHoverJourney = () => {
     })
     .join("");
 };
-
 onHoverJourney();
+
+//Case studies
+const displayCaseStudies = () => {
+  const caseStudies = [
+    {
+      id: 1,
+      company_logo: "./src/svg/caseStudieLogo1.svg",
+      company_name: "Aubuchon hardware",
+      company_title:
+        "Multiple brands in one for largest hardware seller in United States",
+      technology_use_logo: [
+        "./src/svg/logo-adobe.svg",
+        "./src/svg/logo-pwa.svg",
+        "./src/svg/logo-react.svg",
+        "./src/svg/logo-wunderkind.svg",
+        "./src/svg/logo-klevu.svg",
+      ],
+      background_image: "./src/images/caseStudie1.avif",
+      background_color: "bg-emerald-100",
+    },
+    {
+      id: 2,
+      company_logo: "./src/svg/caseStudieLogo2.svg",
+      company_name: "Dunkin",
+      company_title:
+        "Rebuild for one of the most impressive brands in the world",
+      technology_use_logo: [
+        "./src/svg/logo-magento.svg",
+        "./src/svg/logo-aws.svg",
+        "./src/svg/logo-hyva.svg",
+      ],
+      background_image: "./src/images/caseStudie2.avif",
+      background_color: "bg-orange-100",
+    },
+    {
+      id: 3,
+      company_logo: "./src/svg/caseStudieLogo3.svg",
+      company_name: "Gust",
+      company_title: "Building the best university website in Kuwait for Gust",
+      technology_use_logo: [
+        "./src/svg/logo-vercel.svg",
+        "./src/svg/logo-directus.svg",
+        "./src/svg/logo-nextjs.svg",
+        "./src/svg/logo-react.svg",
+        "./src/svg/logo-tailwind.svg",
+      ],
+      background_image: "./src/images/caseStudie3.avif",
+      background_color: "bg-indigo-100",
+    },
+    {
+      id: 4,
+      company_logo: "./src/svg/caseStudiesLogo4.svg",
+      company_name: "Dynamic yield",
+      company_title:
+        "Official integration on Adobe COmmerce for Mastercard's dynamic yield",
+      technology_use_logo: [
+        "./src/svg/logo-adobe.svg",
+        "./src/svg/logo-aws.svg",
+        "./src/svg/logo-dynamic.svg",
+      ],
+      background_image: "./src/images/caseStudie4.avif",
+      background_color: "",
+    },
+  ];
+  const caseStudiesContainers = document.querySelector(
+    "#caseStudiesContainers"
+  );
+  caseStudiesContainers.innerHTML = caseStudies
+    .map((caseStudy) => {
+      const {
+        id,
+        company_logo,
+        company_name,
+        company_title,
+        technology_use_logo,
+        background_image,
+        background_color,
+      } = caseStudy;
+      return `<div key=${id} class="relative my-10">
+            <div
+              class="w-full h-[442px] flex justify-center items-center ${background_color}"
+            >
+              <img
+                class="w-full h-full object-contain object-bottom lg:object-center lg:object-cover"
+                src="${background_image}"
+                alt=""
+              />
+            </div>
+            <div
+              class="w-full flex flex-col justify-center items-start gap-4 px-8 absolute top-6 left-0 md:w-[560px] md:h-[300px] md:justify-between md:py-5"
+            >
+              <img src="${company_logo}" alt="${company_name}">
+              <h3
+                class="text-3xl font-normal text-gray-700 text-left lg:text-4xl"
+              >
+               ${company_title}
+              </h3>
+              <div class="w-full grid grid-cols-5 gap-4">
+                ${technology_use_logo
+                  .map((tech) => {
+                    return `<img class="shrink" src="${tech}" alt="" />`;
+                  })
+                  .join("")}
+              </div>
+            </div>
+          </div>`;
+    })
+    .join("");
+};
+
+displayCaseStudies();
